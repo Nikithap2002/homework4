8e787614cbc5295962781b077d9faccf3edb0e09
Nikithap2002 <padakantinikitha8@gmail.com>
2025-02-13 11:42:53 -0500

    Update homework.R

--- R/homework.R
+++ R/homework.R
@@ -1,13 +1,13 @@
-#' Compute the CDF of a normal mixture distribution
+#'  CDF of a normal mixture distribution
 #'
-#' @param q Numeric value(s) where the CDF is evaluated.
-#' @param mean_1 Mean of the first normal distribution.
-#' @param sd_1 Standard deviation of the first normal distribution.
-#' @param mean_2 Mean of the second normal distribution.
-#' @param sd_2 Standard deviation of the second normal distribution.
-#' @param mixprob Probability weight for the first normal distribution (between 0 and 1).
+#' @param q 
+#' @param mean_1 
+#' @param sd_1 
+#' @param mean_2 
+#' @param sd_2 
+#' @param mixprob 
 #'
-#' @return A numeric value representing the CDF at `q`.
+#' @return 
 #' @export
 pnormmix <- function(q, mean_1, sd_1, mean_2, sd_2, mixprob) {
   p_1 <- pnorm(q, mean = mean_1, sd = sd_1)
@@ -15,11 +15,11 @@ pnormmix <- function(q, mean_1, sd_1, mean_2, sd_2, mixprob) {
   return(mixprob * p_1 + (1 - mixprob) * p_2)
 }
 
-#' Compute the PDF of a normal mixture distribution
+#'PDF of a normal mixture distribution
 #'
-#' @param x Numeric value(s) where the PDF is evaluated.
+#' @param x 
 #' @inheritParams pnormmix
-#' @return A numeric value representing the PDF at `x`.
+#' @return
 #' @export
 dnormmix <- function(x, mean_1, sd_1, mean_2, sd_2, mixprob) {
   d_1 <- dnorm(x, mean = mean_1, sd = sd_1)
@@ -27,11 +27,11 @@ dnormmix <- function(x, mean_1, sd_1, mean_2, sd_2, mixprob) {
   return(mixprob * d_1 + (1 - mixprob) * d_2)
 }
 
-#' Generate random numbers from a normal mixture distribution
+#' random numbers from a normal mixture distribution
 #'
-#' @param n Number of random samples to generate.
+#' @param n 
 #' @inheritParams pnormmix
-#' @return A vector of `n` random values from the mixture distribution.
+#' @return
 #' @export
 rnormmix <- function(n, mean_1, sd_1, mean_2, sd_2, mixprob) {
   u <- runif(n)
@@ -40,11 +40,11 @@ rnormmix <- function(n, mean_1, sd_1, mean_2, sd_2, mixprob) {
   return(x)
 }
 
-#' Compute the quantile function (inverse CDF) of a normal mixture distribution
+#' quantile function (inverse CDF) of a normal mixture distribution
 #'
-#' @param p Probability value(s) for quantile calculation.
+#' @param p 
 #' @inheritParams pnormmix
-#' @return A numeric value representing the quantile at probability `p`.
+#' @return
 #' @export
 qnormmix <- function(p, mean_1, sd_1, mean_2, sd_2, mixprob) {
   quantiles <- numeric(length(p))
